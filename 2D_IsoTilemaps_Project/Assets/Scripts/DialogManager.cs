9c4f67bc433d29fe778213a7c5a287d5a2f99d3d
Artur Melo <arturmcatunda@gmail.com>
2021-04-20 23:53:36 -0300

    Demo 1

---
+++ 2D_IsoTilemaps_Project/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public DialogBox dialogBox;
+    public HUD hud;
+    public Button buttonWardrobe;
+    public Button buttonPC;
+    public Button buttonBed;
+
+    int lastClicked = -1;
+
+    int timeout = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.confirmButton.onClick.AddListener(ClickConfirm);
+        dialogBox.cancelButton.onClick.AddListener(ClickCancel);
+
+		buttonWardrobe.onClick.AddListener(ClickWardrobe);
+		buttonPC.onClick.AddListener(ClickPC);
+		buttonBed.onClick.AddListener(ClickBed);
+    }
+
+    void FixedUpdate(){
+        if(timeout > 0){
+            timeout--;
+            if(timeout == 0){
+                dialogBox.anim.SetBool("IsOpen", false);
+            }
+        }
+    }
+
+    void ClickConfirm(){
+        switch(lastClicked){
+            case 1:
+                hud.updateBars(0.01f, 0.05f, 0.00f, 0.05f, 0.00f);
+                break;
+            case 2:
+                hud.updateBars(0.01f, 0.00f, 0.05f, 0.00f, 0.05f);
+                break;
+            case 3:
+                hud.updateBars(0.00f, -0.03f, -0.03f, -0.03f, -0.03f);
+                break;
+        }
+
+        timeout = 0;
+        dialogBox.anim.SetBool("IsOpen", false);
+    }
+
+    void ClickCancel(){
+        timeout = 0;
+        dialogBox.anim.SetBool("IsOpen", false);
+    }
+
+    void ClickWardrobe(){
+        lastClicked = 1;
+
+        dialogBox.dialogText.text = "É um armário.";
+        dialogBox.confirmButton.GetComponentInChildren<Text>().text = "Abrir";
+        dialogBox.cancelButton.GetComponentInChildren<Text>().text = "Cancelar";
+
+        dialogBox.anim.SetBool("IsOpen", true);
+        timeout = 300;
+    }
+
+    void ClickPC(){
+        lastClicked = 2;
+
+        dialogBox.dialogText.text = "É um computador.";
+        dialogBox.confirmButton.GetComponentInChildren<Text>().text = "Usar";
+        dialogBox.cancelButton.GetComponentInChildren<Text>().text = "Cancelar";
+
+        dialogBox.anim.SetBool("IsOpen", true);
+        timeout = 300;
+    }
+
+    void ClickBed(){
+        lastClicked = 3;
+
+        dialogBox.dialogText.text = "É uma cama.";
+        dialogBox.confirmButton.GetComponentInChildren<Text>().text = "Dormir";
+        dialogBox.cancelButton.GetComponentInChildren<Text>().text = "Cancelar";
+
+        dialogBox.anim.SetBool("IsOpen", true);
+        timeout = 300;
+    }
+}
---
+++ 2D_IsoTilemaps_Project/Assets/Scripts/GenericBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenericBar : MonoBehaviour
+{
+    
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    // public float test = 0.5f;
+    // float aux = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     if(test <= 0f || test >= 1f){
+    //         aux *= -1;
+    //     }
+        
+    //     test += 0.005f * aux;
+
+    //     SetValue(test);
+    // }
+
+    public void SetValue(float newValue){
+        slider.value = newValue;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+    
+    public void IncrementValue(float value){
+        slider.value += value;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
